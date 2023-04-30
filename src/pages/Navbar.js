9e6e0b056ee1a68c4d1a5f3d491338c1d73daf01
majdiShomali <majdi.shomali.1997@gmail.com>
2023-04-30 15:38:49 +0300

    react form task

---
+++ src/pages/Navbar.js
@@ -0,0 +1,33 @@
+import { Outlet, Link } from "react-router-dom";
+import "./css/navbar.css"
+const Navbar = () => {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='logo'>
+        <img src="./logo512.png" alt="logo" />
+        <p>Arabic recipes</p>
+
+        </div>
+        <ul className="navlinks" >
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/SignUp">SignUp</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Navbar;
---
+++ src/pages/SignUp.js
@@ -0,0 +1,7 @@
+import MyForm from "../components/Form";
+
+const Login = () => {
+    return <MyForm/>;
+  };
+  
+  export default Login;
